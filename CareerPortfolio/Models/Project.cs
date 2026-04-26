056b3b2e07d581d25ca5b05ecb8b242548a18612
Jasmine Brown <jas.brown8000@gmail.com>
2026-04-25 22:35:22 -0400

    Added Create and Delete functionality to Project Page

--- CareerPortfolio/Models/Project.cs
+++ CareerPortfolio/Models/Project.cs
@@ -1,11 +1,17 @@
-﻿namespace CareerPortfolio.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CareerPortfolio.Models
 {
     public class Project
     {
         public int ID { get; set; }
+        [Required]
         public string Title { get; set; } = string.Empty;
+        [Required]
         public string Description { get; set; } = string .Empty;
+        [Required]
         public string Status { get; set; } = string.Empty;
+        [Required]
         public string TechStack { get; set; } = string.Empty;
         public string? GitHubUrl { get; set; }
         public string? LiveDemoUrl { get; set; }
--- CareerPortfolio/Pages/Projects.cshtml.cs
+++ CareerPortfolio/Pages/Projects.cshtml.cs
@@ -3,6 +3,7 @@ using CareerPortfolio.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.Json;
 using System.Threading.Tasks;
 
 namespace CareerPortfolio.Pages
@@ -17,9 +18,41 @@ namespace CareerPortfolio.Pages
         }
 
         public List<Project> Projects { get; set; } = new();
+        [BindProperty]
+        public Project NewProject { get; set; } = new();
+
         public async Task OnGet()
         {
            Projects = await _context.Projects.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostCreateAsync() //Razor Pages runs OnPostCreateAsync
+        {
+            if (!ModelState.IsValid) //Check if the form is valid
+            {
+                Projects = await _context.Projects.ToListAsync(); //If invalid, it reloads the project list
+                return Page();//Shows the same page w/errors
+            }
+
+            _context.Projects.Add(NewProject); //If valid, it adds the new project to EF Core
+            await _context.SaveChangesAsync(); // Saves the changes to SQL Server
+
+            return RedirectToPage(); //Redirects back to the Projects page with new project object loaded
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var project = await _context.Projects.FindAsync(id); //Find the project in the database by id
+
+            if (project == null) //If it doesn't exist, just reload the page
+            {
+                return RedirectToPage();
+            }
+
+            _context.Projects.Remove(project); //If it does exist, remove it
+            await _context.SaveChangesAsync(); //Save the database change
+
+            return RedirectToPage(); //Reload the Projects page
         }
     }
 }
